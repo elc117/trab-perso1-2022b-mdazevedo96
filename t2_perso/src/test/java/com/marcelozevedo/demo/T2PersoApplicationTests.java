a537d2eb72550799c97a7e66238db3f3b18f2ac8
mdazevedo96 <42869269+mdazevedo96@users.noreply.github.com>
2022-11-25 10:29:20 -0300

    Add files via upload

---
+++ t2_perso/src/test/java/com/marcelozevedo/demo/T2PersoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcelozevedo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T2PersoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
