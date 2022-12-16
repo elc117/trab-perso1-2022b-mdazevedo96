630d73eebfd1b1ba5a9edf44efa25ac92bdbdcb9
mdazevedo96 <42869269+mdazevedo96@users.noreply.github.com>
2022-12-16 11:06:20 -0300

    Add files via upload

--- src/main/java/org/example/controller/ViewController.java
+++ src/main/java/org/example/controller/ViewController.java
@@ -1,15 +1,16 @@
 package org.example.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ViewController {
+
     @RequestMapping("/")
-    public String home(){
+    public String index(){
         return "index";
     }
-
     @RequestMapping("/pontosturisticos")
     public String pontosturisticos(){
         return "pontosturisticos";
